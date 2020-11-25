24d70fa68efbc703d7da22868d4215c23e70ad49
Laruence Guild <laruenceguild@Laruences-MBP.fios-router.home>
2020-11-25 18:51:37 -0500

    debug info

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
-logger.info("******** S3 BUCKET #{ENV['S3_BUCKET']} *********")
+puts ("******** S3 BUCKET #{ENV['S3_BUCKET']} *********")
 
 # Initialize the Rails application.
 Rails.application.initialize!
