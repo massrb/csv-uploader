c1357ab7d34ce7991a5f95e9ec146cb84ffa5c6e
Laruence Guild <laruenceguild@Laruences-MBP.fios-router.home>
2020-11-25 19:02:26 -0500

    try to fix navbar on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-puts ("******** S3 BUCKET #{ENV['S3_BUCKET']} *********")
-
 # Initialize the Rails application.
 Rails.application.initialize!
