f5cc9729bbdb2ed54ff6bdd09966f1550a83d0a9
Laruence Guild <laruenceguild@Laruences-MBP.fios-router.home>
2020-12-02 19:10:54 -0500

    validation improvements

--- app/lib/csv_parser.rb
+++ app/lib/csv_parser.rb
@@ -6,11 +6,11 @@ class CsvParser
     CSV.foreach(path, headers: true) do |row|
       rnum += 1
       udata = UserDatum.create(row.to_hash.merge({row: rnum}))
-      csv_rec.user_data << udata
-      unless udata.valid?
+      if udata.valid?
+        csv_rec.user_data << udata
+      else
         csv_rec.error_rows << ErrorRow.create(row: rnum,
         error_msg: udata.errors.messages.map{|k,v| "#{k}: #{v[0]}"}.join(", "))
-        udata.destroy
       end
     end
   end
--- app/models/user_datum.rb
+++ app/models/user_datum.rb
@@ -2,30 +2,19 @@
 require_relative '../validators/phone_validator'
 
 class UserDatum < ApplicationRecord
-  validate :names_validation
-  belongs_to :csv_upload
-  validates_with PhoneValidator
   VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_NAME_REGEX = /\A[A-Za-z]{2,}\z/
+  belongs_to :csv_upload, required: false
+  
+  validate :names_validation
+  validates_with PhoneValidator
   validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :first, length: { minimum: 2 }, allow_blank: true, allow_nil: true, format: {with: VALID_NAME_REGEX }
+  validates :last, length: { minimum: 2 }, allow_blank: true, allow_nil: true, format: {with: VALID_NAME_REGEX }
 
   def names_validation
-    last_f = last.to_s.strip
-    first_f = first.to_s.strip
-    unless last_f.empty?
-      if first_f.empty?
-        errors.add(:names, "First name required if last name specified")
-      end
-      check_field(:last, last_f)
-    end
-    check_field(:first, first_f) unless first_f.empty?
-  end
-
-  private
-  def check_field(typ, fld)
-    if fld.length < 2
-      errors.add(typ, "a name field must be at least 2 characters")
-    elsif fld !~ /^[A-Za-z]{2,}/
-      errors.add(typ, "a name field must be alpha")
+    if !last.to_s.strip.empty? && first.to_s.strip.empty?
+      errors.add(:names, "First name required if last name specified")
     end
   end
 end
